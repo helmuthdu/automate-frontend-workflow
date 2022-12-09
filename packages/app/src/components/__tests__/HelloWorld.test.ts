c32107fde11f34c9dd638ec2b235ccde445e0b7c
helmuthdu <helmuthdu@gmail.com>
2022-12-09 21:41:21 +0100

    chore: update dependencies

--- packages/app/src/components/__tests__/HelloWorld.test.ts
+++ packages/app/src/components/__tests__/HelloWorld.test.ts
@@ -1,23 +1,25 @@
 import { mount } from '@vue/test-utils';
 import HelloWorld from '../HelloWorld.vue';
 
-test('mount component', async () => {
-  expect(HelloWorld).toBeTruthy();
+describe('HelloWorld component', () => {
+  it('HelloWorld component', async () => {
+    expect(HelloWorld).toBeTruthy();
 
-  const wrapper = mount(HelloWorld, {
-    props: {
-      count: 4
-    }
+    const wrapper = mount(HelloWorld, {
+      props: {
+        count: 4
+      }
+    });
+
+    expect(wrapper.text()).toContain('4 x 2 = 8');
+    expect(wrapper.html()).toMatchSnapshot();
+
+    await wrapper.get('button').trigger('click');
+
+    expect(wrapper.text()).toContain('4 x 3 = 12');
+
+    await wrapper.get('button').trigger('click');
+
+    expect(wrapper.text()).toContain('4 x 4 = 16');
   });
-
-  expect(wrapper.text()).toContain('4 x 2 = 8');
-  expect(wrapper.html()).toMatchSnapshot();
-
-  await wrapper.get('button').trigger('click');
-
-  expect(wrapper.text()).toContain('4 x 3 = 12');
-
-  await wrapper.get('button').trigger('click');
-
-  expect(wrapper.text()).toContain('4 x 4 = 16');
 });
