e962624b34b00d6d0988b499fb179a647633bbab
helmuthdu <helmuthdu@gmail.com>
2022-12-10 06:56:56 +0100

    chore: update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,19 @@
+import { mergeConfig } from "vite";
+
 module.exports = {
-  "stories": [
-    "../packages/**/*.stories.mdx",
-    "../packages/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  stories: ['../packages/**/*.stories.mdx', '../packages/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/vue3',
+  core: {
+    builder: '@storybook/builder-vite'
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+    previewMdx2: true
+  },
+  async viteFinal(config, options) {
+    return mergeConfig(config, {
+      base: "./",
+    });
+  },
+};
