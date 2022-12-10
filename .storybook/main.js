6b32e024eca6d806f89e9848032ad522982d54a6
helmuthdu <helmuthdu@gmail.com>
2022-12-10 07:16:32 +0100

    chore: update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-import { mergeConfig } from "vite";
-
 module.exports = {
   stories: ['../packages/**/*.stories.mdx', '../packages/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
@@ -12,8 +10,7 @@ module.exports = {
     previewMdx2: true
   },
   async viteFinal(config, options) {
-    return mergeConfig(config, {
-      base: "./",
-    });
+    config.base = "./";
+    return config;
   },
 };
